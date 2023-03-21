0f60fc8f7e22fb788dd5b71a08397f4a248c00b1
ujjwl_kr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-03-21 06:18:17 +0000

    commit

---
+++ Chapter01_Syntax&Variable.js
@@ -0,0 +1,8 @@
+console.log("Hi, this is the introducion to the syntax and variable");
+//comment 
+//java Script statements must end with the semicolon(;). It's not compulsory but it indicates that the statement has ended
+let a=65;
+console.log(a);
+//basically we can take a variable and use it for any data type. There is no restriction in it
+a="Ujjwal";
+console.log(a);
