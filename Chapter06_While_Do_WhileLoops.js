3face44f3b6a91bcdc9b1f2abeb3605f6642f164
ujjwl_kr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-03-25 07:11:07 +0000

    commit

---
+++ Chapter06_While_Do_WhileLoops.js
@@ -0,0 +1,20 @@
+// while loop
+
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n);
+
+let i = 0;
+while(i<n){
+  console.log(i);
+  i++;
+}
+
+// do-while loop
+let n1 = prompt("Enter the value of n1");
+n1 = Number.parseInt(n1);
+
+let j = 10;
+do{
+  console.log(j)
+  j++;
+}while (j < n1) 
