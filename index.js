ac762f6ef7d0e6b66891ca577aaa1e105c0f9cd9
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-04-28 10:36:53 +0530

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'; //using strict we acan hide the bugs of the javascript
+let driversLicense=false;
+const passTest=true;
+
+if(passTest){
+    driversLicense=true;
+}
+if(driversLicense){
+    console.log("I can drive now");
+}
