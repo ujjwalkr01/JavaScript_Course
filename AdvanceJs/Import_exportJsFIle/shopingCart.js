ae3de57b0c0f9947fce5d9cb234ada9179d43d18
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-06-05 17:43:51 +0530

    update

--- AdvanceJs/Import_exportJsFIle/shopingCart.js
+++ AdvanceJs/Import_exportJsFIle/shopingCart.js
@@ -3,7 +3,7 @@
 console.log("Exporting")
 const totalquantity=500;
 const weight=100;
-
+//we can't use export inside any block or function it must be the global variable 
 export const addtoCart=function(product,qt){
   console.log(`You have ${qt} ${product}`);
 }
