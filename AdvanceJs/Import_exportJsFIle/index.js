8a4dde2d3b7596d760bcc9bcd3bf7e25b5c6320c
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-06-12 16:41:45 +0530

    commit

--- AdvanceJs/Import_exportJsFIle/index.js
+++ AdvanceJs/Import_exportJsFIle/index.js
@@ -17,3 +17,30 @@ console.log('Importing');
 shopingCart.addtoCart('bread',5);
 console.log(shopingCart.totalquantity);
 console.log(shopingCart.weight);
+ 
+import cloneDeep from 'lodash-es';
+
+const state={
+    cart:[
+        {product:'bread', quantity:5},
+        {product:'pizza', quantity:5},
+    ],
+    user:{loggedIn: true},
+};
+const stateClone=Object.assign({}, state);
+//here we have made the change in the original object even though by using the cloneDeep we can see no change in the original object 
+const stateDeepClone=cloneDeep(state);
+state.user.loggedIn=false;
+console.log(stateClone);
+
+
+console.log(stateDeepClone);
+console.log(state)
+
+//Note
+/*If we delete the node_module folder then there is no issues we can simply redownload by using the <npm install> and it will download according to the dependencies used in package.json folder
+ */
+
+if(module.hot){
+    module.hot.accept();
+}
